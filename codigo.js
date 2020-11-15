44931aa2e388f3afa1f1244281d4451249f37baa
Brayan Cardozo <brayanalexander95@hotmail.com>
2020-11-15 05:39:52 -0500

    avance carga de puntos y correccion estilo

--- codigo.js
+++ codigo.js
@@ -1,27 +1,34 @@
 
 require([
     "esri/Map", 
-    "esri/views/MapView", 
-    "esri/widgets/BasemapToggle", 
+    "esri/views/MapView",  
     "esri/widgets/BasemapGallery",
-    "esri/layers/FeatureLayer"
-], function (Map, MapView, BasemapGallery, FeatureLayer) {
- 
+    "esri/layers/KMLLayer",
+    "esri/layers/GeoJSONLayer",
+    "esri/layers/Layer"
+
+], function (Map, MapView, BasemapGallery, Layer) {
+
     var map = new Map({
     basemap: "topo-vector"
-    });
-
-  var view = new MapView({
-    container: "viewDiv",
-    map: map,
-    center: [ -74.0817500, 4.6097100], // longitude, latitude
-    zoom: 15
+  
     });
 
  
-
-
-    var basemapGallery = new BasemapGallery({
+    var view = new MapView({
+      container: "viewDiv",
+      map: map,
+      center: [ -74.0817500, 4.6097100], // longitude, latitude
+      zoom: 15
+      });
+  
+      function addLayer(layerItemPromise, index) {
+        return layerItemPromise.then(function (layer) {
+          map.add(layer, index);
+        });
+      }
+      
+      var basemapGallery = new BasemapGallery({
         view: view,
         source: {
           portal: {
@@ -32,31 +39,34 @@ require([
          
       });
       view.ui.remove(basemapGallery, "top-right");
-
+    
         const closeMapGallery = document.getElementById("openMenu")
         closeMapGallery.addEventListener("click", openMapg)
-
+    
         const closeMapGallery2 = document.getElementById("closeMenu")
         closeMapGallery2.addEventListener("click", closeMapg)
         
         function closeMapg(){
-
+    
             view.ui.remove(basemapGallery, "top-right");
             
            
         }
         function openMapg(){
-
+    
             view.ui.add(basemapGallery, "top-right");
             
         }
-   
 
-        var geojsonLayer = new GeoJSONLayer({
-          url: "data2.geojson"
+        var trailheadsPortalItem = Layer.fromPortalItem({
+          portalItem: {
+            id: "b049dfc78a414d13abf33e7f4b8ea971"
+          }
         });
+        addLayer(trailheadsPortalItem, 0);
 
-        map.add(geojsonLayer)
-        
+
 });
+
+
 
